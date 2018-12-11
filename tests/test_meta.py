4defb6b7184c6704d7af957f0d2523762dc0897c
guangtian_li@qq.com <tony.li@gllue.com>
2018-12-11 18:11:08 +0800

    :star2: update tests

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,5 +1,5 @@
 from grpcalchemy.blueprint import Blueprint
-from grpcalchemy.fields import StringField
+from grpcalchemy.fields import StringField, ReferenceField
 from grpcalchemy.meta import __meta__
 from grpcalchemy.orm import Message
 from .test_grpcalchemy import TestGrpcalchemy
@@ -25,8 +25,6 @@ class TestMeta(TestGrpcalchemy):
 
         self.assertEqual("TestMessage", messages[0].name)
         self.assertEqual(1, len(messages[0].fields))
-
-        del __meta__[TestMessage.__filename__]
 
     def test_single_service(self):
         test_blueprint = Blueprint("test")
@@ -57,5 +55,55 @@ class TestMeta(TestGrpcalchemy):
         self.assertEqual("TestMessage", services[0].rpcs[0].request)
         self.assertEqual("TestMessage", services[0].rpcs[0].response)
 
-        del __meta__[TestMessage.__filename__]
-        del __meta__[test_blueprint.file_name]
+    def test_multiple_messages(self):
+        class TestMessageOne(Message):
+            __filename__ = "test"
+            name = StringField()
+
+        class TestMessageTwo(Message):
+            __filename__ = "test"
+            name = StringField()
+
+        class TestMessageThree(Message):
+            name = StringField()
+            ref = ReferenceField(TestMessageOne)
+
+        self.assertEqual(2, len(__meta__.keys()))
+        self.assertEqual(
+            1, len(__meta__[TestMessageThree.__filename__]["import_files"]))
+
+        self.assertEqual(2,
+                         len(__meta__[TestMessageOne.__filename__]["messages"]))
+        self.assertEqual(1,
+                         len(__meta__[TestMessageThree.__filename__]["messages"]))
+
+    def test_multiple_services(self):
+        test_blueprint = Blueprint("test", file_name="test")
+
+        class TestMessage(Message):
+            name = StringField()
+
+        @test_blueprint.register
+        def test_message_one(request: TestMessage, context) -> TestMessage:
+            return TestMessage(test_name=request.name)
+
+        @test_blueprint.register
+        def test_message_two(request: TestMessage, context) -> TestMessage:
+            return TestMessage(test_name=request.name)
+
+        services = __meta__[test_blueprint.file_name]["services"]
+
+        self.assertEqual(1, len(services))
+        self.assertEqual(2, len(services[0].rpcs))
+
+        test_blueprint_two = Blueprint("test", file_name="test")
+
+        @test_blueprint_two.register
+        def test_message_three(request: TestMessage, context) -> TestMessage:
+            return TestMessage(test_name=request.name)
+
+        services = __meta__[test_blueprint.file_name]["services"]
+
+        self.assertEqual(2, len(services))
+        self.assertEqual(2, len(services[0].rpcs))
+        self.assertEqual(1, len(services[1].rpcs))
