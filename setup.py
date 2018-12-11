d7fe4cc355887d690e8bdbc775d15b862b6d7002
guangtian_li@qq.com <tony.li@gllue.com>
2018-12-11 16:47:12 +0800

    update version

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     tests_require=test_requirements,
     url='https://github.com/GuangTianLi/grpcalchemy',
     python_requires='>=3.6.0',
-    version='0.1.1',
+    version='0.1.3',
     zip_safe=False,
 )
