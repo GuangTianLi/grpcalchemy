f609cf3b54a2f66bb0acb442e8ab2f78cc269aa2
tony.li <tony.li@gllue.com>
2018-12-11 16:07:26 +0800

    Set init version

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     tests_require=test_requirements,
     url='https://github.com/GuangTianLi/grpcalchemy',
     python_requires='>=3.6.0',
-    version='0.1.2',
+    version='0.1.0',
     zip_safe=False,
 )
