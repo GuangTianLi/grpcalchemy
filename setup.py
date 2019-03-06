ae7e7c17cf2168b656789869662d61375b48161d
GuangTianLi <guangtian_li@qq.com>
2019-03-06 11:36:23 +0800

    Bump version: 0.2.4 → 0.2.5

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     tests_require=test_requirements,
     url='https://github.com/GuangTianLi/grpcalchemy',
     python_requires='>=3.6.0',
-    version='0.2.4',
+    version='0.2.5',
     zip_safe=False,
 )
