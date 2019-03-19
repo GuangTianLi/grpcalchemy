a625b4b8a11a46bed3ca3e424683c6e59ad2e68e
GuangTianLi <guangtian_li@qq.com>
2019-03-19 11:58:37 +0800

    :star2: release 0.2.7

--- grpcalchemy/__init__.py
+++ grpcalchemy/__init__.py
@@ -9,6 +9,6 @@ __author__ = """GuangTian Li"""
 __email__ = 'guangtian_li@qq.com'
 __version__ = '0.2.6'
 
-from .server import Server
 from .blueprint import Blueprint, Context, current_rpc
 from .globals import current_app
+from .server import Server
