450a30c5416d13507eb2cd6d1c8dd20615304534
GuangTianLi <guangtian_li@qq.com>
2019-03-19 12:24:59 +0800

    :bug: add pyyaml requirement

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["grpcio", "protobuf", "grpcio-tools", "jinja2"]
+requirements = ["grpcio", "protobuf", "grpcio-tools", "jinja2", "pyyaml"]
 
 setup_requirements = []
 
