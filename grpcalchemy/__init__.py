f49b822cd3fff3b27257f11b5ad0bd7e9dc9e0d7
GuangTianLi <guangtian_li@qq.com>
2019-05-22 10:28:50 +0800

    Bump version: 0.2.8 → 0.2.9

--- grpcalchemy/__init__.py
+++ grpcalchemy/__init__.py
@@ -7,7 +7,7 @@
 
 __author__ = """GuangTian Li"""
 __email__ = 'guangtian_li@qq.com'
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 
 from .blueprint import Blueprint, Context, current_rpc
 from .globals import current_app
