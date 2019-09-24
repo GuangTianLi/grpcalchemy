a60ef870527b80493c7a4a10721323ef31b5474d
guangtian.li <guangtian_li@qq.com>
2019-09-24 16:35:47 +0800

    :rocket: fix travis test with bind socket

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ from grpcalchemy.utils import socket_bind_test, select_address_family, get_socka
 class UtilsTestCase(unittest.TestCase):
     def test_socket_bind_test(self):
         host = "0.0.0.0"
-        port = 80
+        port = 50051
         address_family = select_address_family(host)
         server_address = get_sockaddr(host, port, address_family)
         with socket.socket(address_family, socket.SOCK_STREAM) as s:
