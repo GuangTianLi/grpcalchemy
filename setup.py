066c6a6d7189c439afc4d8fb9894c216cd8c12fb
GuangTianLi <guangtian_li@qq.com>
2020-10-28 16:27:30 +0800

    :sparkles: support Python to 3.9

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="The Python micro framework for building gPRC application.",
     install_requires=requirements,
